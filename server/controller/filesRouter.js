ebac836bcfac24893dcf1718da99f4a750eedee4
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-02-18 20:16:16 +0530

    feat: verify route add

--- server/controller/filesRouter.js
+++ server/controller/filesRouter.js
@@ -83,9 +83,85 @@ router.post("/send", upload.array("docs", 12), async (req, res) => {
 
         res.status(200).json({
           success: true,
+          typingPattern,
           signedFile: mz.zip(),
           typingDNAResponse: response,
-          message: "Your files are successfully signed using your typingDNA",
+          message: "Typing pattern successfully embedded in the documents!",
+        });
+      }
+    });
+  } catch (error) {
+    consola.error(error);
+    res
+      .status(500)
+      .send({ success: false, error, message: "internal error occured" });
+  }
+});
+
+router.post("/verify", upload.single("zipFile"), async (req, res) => {
+  const { body, file } = req;
+
+  if (!file) {
+    return res
+      .status(400)
+      .json({ error: "No zip file uploaded. Kindly try again" });
+  }
+
+  try {
+    const { typingPattern } = body;
+
+    if (!typingPattern) {
+      return res.status(400).json({
+        success: false,
+        error: "No typing pattern present. Avoid copy paste of the text",
+      });
+    }
+
+    const mz = new Minizip(file.buffer);
+    let verificationToken = "";
+    let oldHash = "";
+    let newHash = "";
+
+    for (const { filepath, crypt } of mz.list()) {
+      if (filepath === "verify-token.txt") {
+        const extractedFile = mz.extract(filepath);
+        newHash = hasha(extractedFile);
+        verificationToken = extractedFile.toString();
+      }
+      if (filepath === "tokenHash.txt") {
+        const extractedFile = mz.extract(filepath);
+        oldHash = hasha(extractedFile);
+      }
+    }
+
+    const tokens = verificationToken.split("\n").filter((ele) => ele != null);
+    consola.info(tokens);
+    const [userId, oldTypingPattern, hashes] = tokens;
+
+    consola.info("verifying for userId: ", userId);
+
+    typingDnaClient.auto(userId, typingPattern, (err, response) => {
+      if (err) {
+        consola.warn(err);
+        return res.status(500).json({
+          success: false,
+          error: err,
+          data: "Internal Server Erorr. Please try again",
+        });
+      } else {
+        consola.info(JSON.stringify(response));
+        if (response.statusCode !== 200) {
+          return res.status(response.statusCode).json({
+            name: response.name,
+            message: response.message,
+          });
+        }
+
+        res.status(200).json({
+          success: true,
+          typingPattern,
+          typingDNAResponse: response,
+          message: "Signed-zip file verification is complete!",
         });
       }
     });
