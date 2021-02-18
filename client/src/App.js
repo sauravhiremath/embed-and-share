b60d13d132af9e1e9a5a6c16d1db7e7492577206
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-02-18 15:29:26 +0530

    feat: add geist wrapper

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Landing from "./containers/Landing/Landing";
+
+function App() {
+  return (
+    <div className="app">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
