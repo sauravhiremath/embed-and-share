d3dd34d19ea7d39e099c56114306c3a6bba6b074
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-03-26 17:15:36 +0530

    feat: custom footer

--- client/src/containers/Footer/Footer.js
+++ client/src/containers/Footer/Footer.js
@@ -1,17 +1,132 @@
-import { Grid, Link } from "@geist-ui/react";
+import "./footer.css";
+import { Grid, Link, Text } from "@geist-ui/react";
+import { CustomUnderline } from "./customUnderline";
 
 function Footer() {
   return (
-    <Grid.Container justify="center">
-      <Grid sm={24} justify="center">
-        <Link
-          color
-          block
-          href="https://github.com/sauravhiremath/embed-and-share"
-          icon
-        >
-          © https://github.com/sauravhiremath/embed-and-share
-        </Link>
+    <Grid.Container justify="space-between" gap={3} className="site-footer">
+      <Grid sm={24} md={12}>
+        <Grid.Container>
+          <Grid xs={24}>
+            <Text h4>ABOUT ME</Text>
+          </Grid>
+          <Grid xs={24} md={16}>
+            <Text span className="about-me ">
+              A friendly neighborhood developer. I am a third year Computer
+              Science Engineer from India. Previous experience in{" "}
+              <CustomUnderline text="Full Stack Dev" /> (Primarily React, Node,
+              GoLang, Python), <CustomUnderline text="DevOps Ninja 🥷" />,
+              Cyber-security, NLP, and other misc. stuff
+            </Text>
+          </Grid>
+        </Grid.Container>
+      </Grid>
+      <Grid xs={24} md={6}>
+        <Grid.Container justify="space-around">
+          <Grid xs={24}>
+            <Text h4 className="contact-me">
+              CONTACT ME
+            </Text>
+          </Grid>
+          <Grid xs={12} md={12}>
+            <Text span>
+              <Link
+                className="linker"
+                icon
+                block
+                href="https://github.com/sauravhiremath"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Github
+              </Link>
+              <br />
+              <Link
+                className="linker"
+                icon
+                block
+                href="https://www.linkedin.com/in/sauravmh/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                LinkedIn
+              </Link>
+              <br />
+              <Link
+                className="linker"
+                icon
+                block
+                href="https://twitter.com/sauravmh"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Twitter
+              </Link>
+            </Text>
+          </Grid>
+          <Grid xs={12} md={12}>
+            <Text span>
+              <Link
+                className="linker"
+                icon
+                block
+                href="https://sauravmh.com/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Portfolio
+              </Link>
+              <br />
+              <Link
+                className="linker"
+                icon
+                block
+                href="http://blog.sauravmh.com/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                My Blog
+              </Link>
+              <br />
+              <Link
+                className="linker"
+                icon
+                block
+                href="https://medium.com/@sauravmh"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Medium
+              </Link>
+              <br />
+              <Link
+                className="linker"
+                icon
+                block
+                href="http://dev.to/sauravmh"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Dev.to
+              </Link>
+            </Text>
+          </Grid>
+        </Grid.Container>
+      </Grid>
+      <Grid xs={24}>
+        <Grid.Container justify="center">
+          <Grid>
+            <Link
+              icon
+              block
+              href="https://github.com/sauravhiremath/embed-and-share"
+              rel="noreferrer"
+              target="_blank"
+            >
+              © github/embed-and-share
+            </Link>
+          </Grid>
+        </Grid.Container>
       </Grid>
     </Grid.Container>
   );
---
+++ client/src/containers/Footer/customUnderline.js
@@ -0,0 +1,29 @@
+import "./footer.css";
+
+export function CustomUnderline({ text, link, style }) {
+  return (
+    <div style={{ display: "inline-flex" }}>
+      <div class="svg-wrap">
+        <svg viewBox="0 0 400 60" xmlns="http://www.w3.org/2000/svg">
+          <path
+            id="svg_marker"
+            d="m 3.518915,27.827324 c 55.429038,4.081 111.581115,5.822 167.117815,2.867 22.70911,-1.208 45.39827,-0.601 68.126,-0.778 28.38172,-0.223 56.76078,-1.024 85.13721,-1.33 24.17378,-0.261 48.4273,0.571 72.58114,0.571"
+          ></path>
+        </svg>
+      </div>
+      <section class="link-svgmarker">
+        <span
+          href={link}
+          rel="noreferrer"
+          target="_blank"
+          style={{ fontWeight: 600, ...style }}
+        >
+          {text}
+          <svg class="link-svgline">
+            <use xlinkHref="#svg_marker"></use>
+          </svg>
+        </span>
+      </section>
+    </div>
+  );
+}
